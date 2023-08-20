eaeeef762abbe79354c1be6a2f1ab46ac149cbe6
JINESHK7741 <jineshkhandelwal1906@gmail.com>
2023-08-20 13:22:52 +0530

    changes in index.js and app.js

--- 01.basic-rect/src/App.js
+++ 01.basic-rect/src/App.js
@@ -2,7 +2,7 @@
 
 function App() {
   return (
-    <h1>REACT-JS</h1>
+    <h1>REACT-JS|I have learned about package .json files and I learned abot difference to create project by creat-react-app and vite.In create-react-app we get a heavy bundles with all unnecessary packages But with vite we get only react and react-dom which is important for Webpages.So now I have clear understanding baout create-react-app and vite utility.</h1>
   );
 }
 
