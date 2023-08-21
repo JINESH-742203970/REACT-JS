dbf19ab8764c3fdeab2e2409c0538ec68f15b94d
JINESHK7741 <jineshkhandelwal1906@gmail.com>
2023-08-21 22:49:45 +0530

    Final commit

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-  
+  function MyApp(){
+    return(
+      <h2>Chai aur Hitesh</h2>
+    )
+  }
 
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     {/* <App /> */
+//       MyApp()
+//     }
+//   </React.StrictMode>,
+// )
+const anotherElement=(
+  <a href="https://google.com" target='_blank'>Visit google</a>
+)
+
+const reactElement=React.createElement(
+  'a',//Tag
+  {href:'https://google.com',target:'blank'},//Attribute
+  'click me to visit google'//Content
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+  // anotherElement
+  <App/>
 )
